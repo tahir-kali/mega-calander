511dcea37c96d1c112a06034b8047a97673515cc
Codeholic <irenic.tahir@gmail.com>
2023-04-21 11:49:43 +0500

    Initial Commit in Eidul Fitr

---
+++ CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  
+//  mega-calander
+//
+//  Created by Tahir Rahimi 21 April, 2023
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
